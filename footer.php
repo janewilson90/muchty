e38e1200e6c19d1bc42018745632734ec4839223
Jane Wilson <jane.wilson@evogi.coim>
2015-12-10 16:03:55 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+      <footer>
+        <div class="row">
+          <div class="container-fluid">
+            <div class="col-md-6">
+              <h4>British Dental Association</h4>
+              <ul class="link-list">
+                <li><a href="#">List 1</a></li>
+                <li><a href="#">List 1</a></li>
+                <li><a href="#">List 1</a></li>
+                <li><a href="#">List 1</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <h4>Required Links</h4>
+            </div>
+            <div class="col-md-2">
+              <h4>Copyright stuff</h4>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    </body>
+    </html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php include("header.php"); ?>
+
+      <div class="row">
+        <div class="container-fluid">
+          <div class="jumbotron">
+            <h1>Catering for your needs</h1>
+            <p>...</p>
+            <p><button class="casper" href="#" role="button">Learn more</button></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="container-fluid">
+
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-user-md"></i> NHS Patients</h3>
+              </div>
+              <div class="panel-body">
+                Panel content
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-gbp"></i> Private Patients</h3>
+              </div>
+              <div class="panel-body">
+                Panel content
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-smile-o"></i> Childsmile</h3>
+              </div>
+              <div class="panel-body">
+                Panel content
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+<?php include("footer.php"); ?>
